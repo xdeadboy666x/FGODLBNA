8848cc8e106e4da7d2ec8f029fcd4efe6f08cf4b
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-04-28 08:18:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ fate_region = os.environ['fateRegion']
 webhook_discord_url = os.environ['webhookDiscord']
 blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
 
-
 UA = os.environ['UserAgent']
 
 if UA:
@@ -31,13 +30,12 @@ coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 def check_blue_apple_cron(instance):
     if blue_apple_cron:
-
         cron = croniter(blue_apple_cron)
         next_date = cron.get_next(datetime)
         current_date = datetime.now()
-        
+
         if current_date >= next_date:
-            logger.info('Trying buy one blue apple!')
+            logger.info('Exchanging Blue Fruit!')
             instance.buyBlueApple(1)
             time.sleep(2)
 
@@ -58,14 +56,14 @@ def get_latest_verCode():
 
 def main():
     if userNums == authKeyNums and userNums == secretKeyNums:
-        logger.info('Getting Latest Assets Info')
+        logger.info('Fetching Game Data')
         fgourl.set_latest_assets()
 
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info('Login in...!')
+                logger.info('Login in...')
                 instance.topLogin()
                 time.sleep(2)
                 instance.topHome()
@@ -80,11 +78,11 @@ def main():
                     instance.buyBlueApple(1)
                     time.sleep(2)
                     for _ in range(3):
-                        instance.buyBlueApple(1) 
+                        instance.buyBlueApple(1)
                         time.sleep(2)
                 except Exception as ex:
                     logger.error(ex)
-                    
+
             except Exception as ex:
                 logger.error(ex)
 
