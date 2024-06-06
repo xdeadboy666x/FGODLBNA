05754fd53996ee8cecc47746a942e5f373e6a4b5
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-05 22:10:03 -0600

    Update GetSubGachaId.py

--- libs/GetSubGachaId.py
+++ libs/GetSubGachaId.py
@@ -51,3 +51,4 @@ def GetGachaSubIdFP(region, userQuest: List = []):
             return gacha["id"]
         elif gacha["commonReleaseId"] == 0:
             return gacha["id"]
+    return 0  # If no event or commonReleaseId is matched, return 0
