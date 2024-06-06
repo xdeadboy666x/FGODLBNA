4f9b25148531503fb6afb27ffa1db63bef39b163
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-05 22:15:26 -0600

    Update GetSubGachaId.py

--- libs/GetSubGachaId.py
+++ libs/GetSubGachaId.py
@@ -1,31 +1,25 @@
 from typing import List
-
 import requests
-
 from mytime import GetTimeStamp
-
 
 def isMatchedQuestCondition(region: str, userQuest: List, commonReleaseId: int):
     api = f"https://api.atlasacademy.io/nice/{region}/common-release/{commonReleaseId}"
     response = requests.get(api)
     response.raise_for_status()
     condition = response.json()
-    if len(condition) > 0 and condition[0].get("condType", "") == "questClear":
+    if condition and condition[0].get("condType", "") == "questClear":
+        condQuestId = condition[0]["condId"]
+        condClearNum = condition[0]["condNum"]
         for quest in userQuest:
-            condQuestId = condition[0]["condId"]
-            condClearNum = condition[0]["condNum"]
-            # Don't know quest["clearNum"] equl condClearNum is fine or not
             if quest["questId"] == condQuestId and quest["clearNum"] > condClearNum:
                 return True
             elif quest["questId"] == condQuestId and quest["clearNum"] <= condClearNum:
                 return False
         return False
     else:
-        # please implement webhook sned error or etc here
+        # Placeholder for webhook error handling
         pass
 
-
-# Get Friend Summon Gacha Sub Id
 def GetGachaSubIdFP(region, userQuest: List = []):
     api = f"https://git.atlasacademy.io/atlasacademy/fgo-game-data/raw/branch/NA/master/mstGachaSub.json"
     response = requests.get(api)
@@ -33,22 +27,20 @@ def GetGachaSubIdFP(region, userQuest: List = []):
     data.reverse()
 
     timeNow = GetTimeStamp()
-    actiaveGacha = []
+    activeGacha = []
     for gacha in data:
         if gacha["closedAt"] > timeNow and gacha["openedAt"] < timeNow:
-            actiaveGacha.append(gacha)
+            activeGacha.append(gacha)
         else:
             break
 
-    if len(actiaveGacha) == 0:
-        # please implement webhook sned error or etc here
+    if not activeGacha:
+        # Placeholder for webhook error handling
         pass
 
-    # Start by sorting from highest to lowest priority, then within the same priority, sort by commonReleaseId
-    sortedGachaList = sorted(actiaveGacha, key=lambda x: (x["priority"], x["commonReleaseId"]), reverse=True)
+    sortedGachaList = sorted(activeGacha, key=lambda x: (x["priority"], x["commonReleaseId"]), reverse=True)
     for gacha in sortedGachaList:
         if gacha["commonReleaseId"] != 0 and isMatchedQuestCondition(region, userQuest, gacha["commonReleaseId"]):
             return gacha["id"]
         elif gacha["commonReleaseId"] == 0:
             return gacha["id"]
-    return 0  # If no event or commonReleaseId is matched, return 0
