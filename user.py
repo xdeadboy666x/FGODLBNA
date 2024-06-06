88ae301ca06779024128c0f1a74f1db3fb4bad52
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-06 13:58:40 -0600

    Update user.py

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import msgpack
 import uuid
 import hashlib
 import base64
@@ -7,6 +8,15 @@ import mytime
 import gacha
 import webhook
 import main
+import logging
+import json
+import os
+import subprocess
+import re
+import sys
+import binascii
+import random
+import time
 
 from urllib.parse import quote_plus
 from libs.GetSubGachaId import GetGachaSubIdFP
@@ -69,10 +79,18 @@ class ParameterBuilder:
 
 
 class Rewards:
-    def __init__(self, stone, level, ticket):
+    def __init__(self, stone, level, ticket, goldenfruit, silverfruit, bronzefruit, bluebronzesapling, bluebronzefruit, pureprism, sqf01, holygrail):
         self.stone = stone
         self.level = level
         self.ticket = ticket
+        self.goldenfruit = goldenfruit
+        self.silverfruit = silverfruit
+        self.bronzefruit = bronzefruit
+        self.bluebronzesapling = bluebronzesapling
+        self.bluebronzefruit = bluebronzefruit
+        self.pureprism = pureprism
+        self.sqf01 = sqf01
+        self.holygrail = holygrail
 
 
 class Login:
@@ -117,24 +135,79 @@ class user:
 
         self.builder_.AddParameter(
             'assetbundleFolder', fgourl.asset_bundle_folder_)
+        self.builder_.AddParameter('deviceInfo', 'HUAWEI MAR-LX3Bm / Android OS 10 / API-29 (MAR-L03B 10.0.0.274(C69E7R1P1)/10.0.0.274C69)')
         self.builder_.AddParameter('isTerminalLogin', '1')
         self.builder_.AddParameter('userState', str(userState))
 
         data = self.Post(
             f'{fgourl.server_addr_}/login/top?_userId={self.user_id_}')
 
+        responses = data['response']
+        
+        with open('login.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
         self.name_ = hashlib.md5(
             data['cache']['replaced']['userGame'][0]['name'].encode('utf-8')).hexdigest()
         stone = data['cache']['replaced']['userGame'][0]['stone']
         lv = data['cache']['replaced']['userGame'][0]['lv']
         ticket = 0
+        goldenfruit = 0
+        silverfruit = 0
+        bronzefruit = 0
+        bluebronzesapling = 0
+        bluebronzefruit = 0
+        pureprism = 0
+        sqf01 = 0
+        holygrail = 0
 
         for item in data['cache']['replaced']['userItem']:
             if item['itemId'] == 4001:
                 ticket = item['num']
                 break
+        
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 100:
+                goldenfruit = item['num']
+                break
 
-        rewards = Rewards(stone, lv, ticket)
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 101:
+                silverfruit = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 102:
+                bronzefruit = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 103:
+                bluebronzesapling = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 104:
+                bluebronzefruit = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 46:
+                pureprism = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 16:
+                sqf01 = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 7999:
+                holygrail = item['num']
+                break
+
+        
+        rewards = Rewards(stone, lv, ticket, goldenfruit, silverfruit, bronzefruit, bluebronzesapling, bluebronzefruit, pureprism, sqf01, holygrail)
 
         DataWebhook.append(rewards)
 
@@ -187,6 +260,166 @@ class user:
 
         webhook.topLogin(DataWebhook)
 
+    def topLogin2(self):
+        DataWebhook = []  # This data will be use in discord webhook!
+
+        lastAccessTime = self.builder_.parameter_list_[5][1]
+        userState = (-int(lastAccessTime) >>
+                     2) ^ self.user_id_ & fgourl.data_server_folder_crc_
+
+        self.builder_.AddParameter(
+            'assetbundleFolder', fgourl.asset_bundle_folder_)
+        self.builder_.AddParameter('deviceInfo', 'Google Pixel 5 / Android OS 14 / API-34 (UP1A.231105.001/10817346)')
+        self.builder_.AddParameter('isTerminalLogin', '1')
+        self.builder_.AddParameter('userState', str(userState))
+
+        data = self.Post(
+            f'{fgourl.server_addr_}/login/top?_userId={self.user_id_}')
+
+        responses = data['response']
+        
+        with open('login.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+        self.name_ = hashlib.md5(
+            data['cache']['replaced']['userGame'][0]['name'].encode('utf-8')).hexdigest()
+        stone = data['cache']['replaced']['userGame'][0]['stone']
+        lv = data['cache']['replaced']['userGame'][0]['lv']
+        ticket = 0
+        goldenfruit = 0
+        silverfruit = 0
+        bronzefruit = 0
+        bluebronzesapling = 0
+        bluebronzefruit = 0
+        pureprism = 0
+        sqf01 = 0
+        holygrail = 0
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 4001:
+                ticket = item['num']
+                break
+        
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 100:
+                goldenfruit = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 101:
+                silverfruit = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 102:
+                bronzefruit = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 103:
+                bluebronzesapling = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 104:
+                bluebronzefruit = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 46:
+                pureprism = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 16:
+                sqf01 = item['num']
+                break
+
+        for item in data['cache']['replaced']['userItem']:
+            if item['itemId'] == 7999:
+                holygrail = item['num']
+                break
+
+        
+        rewards = Rewards(stone, lv, ticket, goldenfruit, silverfruit, bronzefruit, bluebronzesapling, bluebronzefruit, pureprism, sqf01, holygrail)
+
+        DataWebhook.append(rewards)
+
+        login_days = data['cache']['updated']['userLogin'][0]['seqLoginCount']
+        total_days = data['cache']['updated']['userLogin'][0]['totalLoginCount']
+
+        act_max = data['cache']['replaced']['userGame'][0]['actMax']
+        act_recover_at = data['cache']['replaced']['userGame'][0]['actRecoverAt']
+        now_act = (act_max - (act_recover_at - mytime.GetTimeStamp()) / 300)
+
+        add_fp = data['response'][0]['success']['addFriendPoint']
+        total_fp = data['cache']['replaced']['tblUserGame'][0]['friendPoint']
+
+        login = Login(
+            self.name_,
+            login_days,
+            total_days,
+            act_max, act_recover_at,
+            now_act,
+            add_fp,
+            total_fp
+        )
+
+        DataWebhook.append(login)
+
+        if 'seqLoginBonus' in data['response'][0]['success']:
+            bonus_message = data['response'][0]['success']['seqLoginBonus'][0]['message']
+
+            items = []
+            items_camp_bonus = []
+
+            for i in data['response'][0]['success']['seqLoginBonus'][0]['items']:
+                items.append(f'{i["name"]} x{i["num"]}')
+
+            if 'campaignbonus' in data['response'][0]['success']:
+                bonus_name = data['response'][0]['success']['campaignbonus'][0]['name']
+                bonus_detail = data['response'][0]['success']['campaignbonus'][0]['detail']
+
+                for i in data['response'][0]['success']['campaignbonus'][0]['items']:
+                    items_camp_bonus.append(f'{i["name"]} x{i["num"]}')
+            else:
+                bonus_name = None
+                bonus_detail = None
+
+            bonus = Bonus(bonus_message, items, bonus_name,
+                          bonus_detail, items_camp_bonus)
+            DataWebhook.append(bonus)
+        else:
+            DataWebhook.append("No Bonus")
+
+
+    def buyBlueApple(self, quantity=1):
+        # https://game.fate-go.jp/shop/purchase
+        
+        
+        self.builder_.AddParameter('id', '13000000')
+        self.builder_.AddParameter('num', str(quantity))
+
+        data = self.Post(f'{fgourl.server_addr_}/shop/purchase?_userId={self.user_id_}')
+        responses = data['response']
+
+        for response in responses:
+            resCode = response['resCode']
+            resSuccess = response['success']
+            nid = response["nid"]
+
+            if (resCode != "00"):
+                continue
+
+            if nid == "purchase":
+                if "purchaseName" in resSuccess and "purchaseNum" in resSuccess:
+                    purchaseName = resSuccess['purchaseName']
+                    purchaseNum = resSuccess['purchaseNum']
+
+                    main.logger.info(f"{purchaseNum}x {purchaseName} purchased!")
+                    webhook.shop(purchaseName, purchaseNum)
+    
+
     def drawFP(self):
         self.builder_.AddParameter('storyAdjustIds', '[]')
         self.builder_.AddParameter('gachaId', '1')
@@ -197,16 +430,9 @@ class user:
         if main.fate_region == "NA":
             gachaSubId = GetGachaSubIdFP("NA")
             if gachaSubId is None:
-                gachaSubId = "0"  # or any other default value as a string
+                gachaSubId = "0"
             self.builder_.AddParameter('gachaSubId', gachaSubId)
             main.logger.info(f"Friend Point Gacha Sub Id " + gachaSubId)
-        else:
-            gachaSubId = GetGachaSubIdFP("JP")
-            if gachaSubId is None:
-                gachaSubId = "0"  # or any other default value as a string
-            self.builder_.AddParameter('gachaSubId', gachaSubId)
-            main.logger.info(f"Friend Point Gacha Sub Id " + gachaSubId)
-
 
         data = self.Post(
             f'{fgourl.server_addr_}/gacha/draw?_userId={self.user_id_}')
@@ -243,3 +469,54 @@ class user:
 
     def topHome(self):
         self.Post(f'{fgourl.server_addr_}/home/top?_userId={self.user_id_}')
+
+
+    def lq001(self):
+         # https://game.fate-go.jp/present/list?_userId=
+          
+        data = self.Post(
+            f'{fgourl.server_addr_}/present/list?_userId={self.user_id_}')
+        
+        responses = data['response']
+        main.logger.info(f"Getting rewards!")
+
+    def lq002(self):
+         # https://game.fate-go.jp/present/receive?_userId=
+        
+        with open('login.json', 'r', encoding='utf-8')as f:
+            data = json.load(f)
+
+        present_ids = []
+        for item in data['cache']['replaced']['userPresentBox']:
+            if item['objectId'] in [2, 6, 11, 16, 3, 46, 18, 48, 4001, 100, 101, 102, 103, 104, 1, 4, 7998, 7999, 1000, 2000]:
+                present_ids.append(str(item['presentId']))
+
+        with open('JJM.json', 'w') as f:
+            json.dump(present_ids, f, ensure_ascii=False, indent=4)
+
+        main.logger.info(f"Parsing!")
+
+        time.sleep(1)
+
+        if os.path.exists('JJM.json'):
+            with open('JJM.json', 'r', encoding='utf-8') as file:
+                data1 = json.load(file)
+
+            data = data1
+
+            msgpack_data = msgpack.packb(data)
+
+            base64_encoded_data = base64.b64encode(msgpack_data).decode()
+
+            self.builder_.AddParameter('presentIds', base64_encoded_data)
+            self.builder_.AddParameter('itemSelectIdx', '0')
+            self.builder_.AddParameter('itemSelectNum', '0')
+
+            data = self.Post(
+                f'{fgourl.server_addr_}/present/receive?_userId={self.user_id_}')
+    
+            responses = data['response']
+
+            main.logger.info(f"Claimed rewards!")
+        else:
+            main.logger.info(f"No rewards at the moment!")
