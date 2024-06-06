3726405cf527749f9d25bede6fa5ce82748f5b4f
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-06 00:18:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,16 @@ import fgourl
 import user
 import coloredlogs
 import logging
+from datetime import datetime
+from croniter import croniter
 
+# Environment Variables
 userIds = os.environ['userIds'].split(',')
 authKeys = os.environ['authKeys'].split(',')
 secretKeys = os.environ['secretKeys'].split(',')
 fate_region = os.environ['fateRegion']
 webhook_discord_url = os.environ['webhookDiscord']
+blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
 
 UA = os.environ['UserAgent']
 
@@ -25,41 +29,60 @@ secretKeyNums = len(secretKeys)
 logger = logging.getLogger("FGO Daily Login")
 coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
+
+def check_blue_apple_cron(instance):
+    if blue_apple_cron:
+        cron = croniter(blue_apple_cron)
+        next_date = cron.get_next(datetime)
+        current_date = datetime.now()
+
+        if current_date >= next_date:
+            logger.info('Exchanging Blue Fruit!')
+            instance.buyBlueApple(1)
+            time.sleep(2)
+
+
 def get_latest_verCode():
     endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-VerCode-extractor/NA/VerCode.json"
+
     response = requests.get(endpoint).text
     response_data = json.loads(response)
 
     return response_data['verCode']
 
 
-
 def main():
     if userNums == authKeyNums and userNums == secretKeyNums:
+        logger.info('Fetching Game Data')
         fgourl.set_latest_assets()
+
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info(f"\n ======================================== \n [+] Login \n ======================================== " )
-
-                time.sleep(1)
-                instance.topLogin_s()
+                logger.info('Loggin in...')
+                instance.topLogin()
                 time.sleep(2)
                 instance.topHome()
                 time.sleep(2)
                 instance.lq001()
                 instance.lq002()
                 time.sleep(2)
-                instance.buyBlueApple()
-                time.sleep(1)
-                instance.lq003()
-                time.sleep(1)
-                instance.drawFP()
 
+                check_blue_apple_cron(instance)
+                logger.info('Exchanging Blue Fruit!')
+                try:
+                    instance.buyBlueApple(1)
+                    time.sleep(2)
+                    for _ in range(3):
+                        instance.buyBlueApple(1)
+                        time.sleep(2)
+                except Exception as ex:
+                    logger.error(ex)
 
             except Exception as ex:
                 logger.error(ex)
+
 
 if __name__ == "__main__":
     main()
