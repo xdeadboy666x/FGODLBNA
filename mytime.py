2ba93ca87c62aa0363ff10e7f2b2c7794ce6f9e7
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-17 16:01:13 -0600

    Update mytime.py

--- mytime.py
+++ mytime.py
@@ -1,21 +1,21 @@
 from datetime import datetime, timedelta, timezone
 
-tz_utc_plus_9 = timezone(timedelta(hours=9))
+tz_game_server = timezone(timedelta(hours=9))
 
 def GetNowTimeHour():
-    return datetime.now(tz=tz_utc_plus_9).hour
+    return datetime.now(tz=tz_game_server).hour
 
 def GetNowTime():
-    return datetime.now(tz=tz_utc_plus_9)
+    return datetime.now(tz=tz_game_server)
 
 def GetFormattedNowTime():
-    return datetime.now(tz=tz_utc_plus_9).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.now(tz=tz_game_server).strftime('%Y-%m-%d %H:%M:%S')
 
 def GetTimeStamp():
-    return int(datetime.now(tz=tz_utc_plus_9).timestamp())
+    return int(datetime.now(tz=tz_game_server).timestamp())
 
 def TimeStampToString(timestamp):
-    return datetime.fromtimestamp(timestamp, tz=tz_utc_plus_9)
+    return datetime.fromtimestamp(timestamp, tz=tz_game_server)
 
 def GetNowTimeFileName():
-    return datetime.now(tz=tz_utc_plus_9).strftime('%Y/%m/%d.log')
+    return datetime.now(tz=tz_game_server).strftime('%Y/%m/%d.log')
