596148c98bb0f8126a38d244687a10dc7e001885
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-17 00:17:23 -0600

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -32,7 +32,7 @@ def topLogin(data: list) -> None:
         "embeds": [
             {
                 "title": "FGO Login System - " + main.fate_region,
-                "description": f"Login successful. Displaying Master details.\n\n{messageBonus}",
+                "description": f"Details.\n\n{messageBonus}",
                 "color": 563455,
                 "fields": [
                     {"name": "Master Name", "value": f"{name1}", "inline": True},
@@ -46,16 +46,15 @@ def topLogin(data: list) -> None:
                     {"name": "Bronze Fruit", "value": f"{rewards.bronzefruit}", "inline": True},
                     {"name": "Bronzed Cobalt Fruit", "value": f"{rewards.bluebronzefruit}", "inline": True},
                     {"name": "Bronze Sapling", "value": f"{rewards.bluebronzesapling}", "inline": True},
-                    {"name": "Consecutive Login Days", "value": f"{login.login_days}", "inline": True},
-                    {"name": "Total Login Days", "value": f"{login.total_days}", "inline": True},
-                    {"name": "Mana Prism", "value": f"{rewards.pureprism}", "inline": True},
-                    {"name": "Friend Points", "value": f"{login.total_fp}", "inline": True},
-                    {"name": "Friend Points Earned Today", "value": f"+{login.add_fp}", "inline": True},
-                    {"name": "Current AP", "value": f"{login.remaining_ap}", "inline": True},
+                    {"name": "Total Login Days", "value": f"{login.login_days} \/ {login.total_days}", "inline": True},
+                    {"name": "Pure Prism", "value": f"{rewards.pureprism}", "inline": True},
+                    {"name": "FP", "value": f"{login.total_fp}", "inline": True},
+                    {"name": "Gained FP", "value": f"+{login.add_fp}", "inline": True},
+                    #{"name": "Current AP", "value": f"{login.remaining_ap}", "inline": True},
                     {"name": "Holy Grail", "value": f"{rewards.holygrail}", "inline": True},
                 ],
                 "thumbnail": {
-                    "url": "https://www.fate-go.jp/manga_fgo/images/commnet_chara02.png"
+                    "url": "https://www.fate-go.jp/manga_fgo/images/commnet_chara16.png"
                 }
             }
         ],
@@ -117,13 +116,13 @@ def drawFP(servants, missions) -> None:
         "embeds": [
             {
                 "title": "FGO Auto Summoning System - " + main.fate_region,
-                "description": f"Completed daily free Friend Point summon. Displaying summon results.\n\n{message_mission}",
+                "description": f"Gacha results.\n\n{message_mission}",
                 "color": 5750876,
                 "fields": [
                     {"name": "Friend Point Summon Pool", "value": f"{message_servant}", "inline": False}
                 ],
                 "thumbnail": {
-                    "url": "https://www.fate-go.jp/manga_fgo/images/commnet_chara02_rv.png"
+                    "url": "https://www.fate-go.jp/manga_fgo/images/commnet_chara04_rv.png"
                 }
             }
         ],
