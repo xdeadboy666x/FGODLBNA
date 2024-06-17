033891d7c588675ad840fbc093dbc83cbe15094b
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-17 12:02:36 -0600

    Update mytime.py

--- mytime.py
+++ mytime.py
@@ -1,22 +1,21 @@
 from datetime import datetime, timedelta, timezone
 
-tz_utc_6 = timezone(timedelta(hours=6))
-
+tz_utc_minus_6 = timezone(timedelta(hours=-6))
 
 def GetNowTimeHour():
-    return datetime.now(tz=tz_utc_6).hour
+    return datetime.now(tz=tz_utc_minus_6).hour
 
 def GetNowTime():
-    return datetime.now(tz=tz_utc_6)
+    return datetime.now(tz=tz_utc_minus_6)
 
 def GetFormattedNowTime():
-    return datetime.now(tz=tz_utc_6).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.now(tz=tz_utc_minus_6).strftime('%Y-%m-%d %H:%M:%S')
 
 def GetTimeStamp():
-    return int(datetime.now(tz=tz_utc_6).timestamp())
+    return int(datetime.now(tz=tz_utc_minus_6).timestamp())
 
 def TimeStampToString(timestamp):
-    return datetime.fromtimestamp(timestamp, tz=tz_utc_6)
+    return datetime.fromtimestamp(timestamp, tz=tz_utc_minus_6)
 
 def GetNowTimeFileName():
-    return datetime.now(tz=tz_utc_6).strftime('%Y/%m/%d.log')
+    return datetime.now(tz=tz_utc_minus_6).strftime('%Y/%m/%d.log')
