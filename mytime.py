4dbf7860a4dfe3cf1770d2e2eadd2937e8622b86
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-21 01:10:47 -0600

    Update mytime.py

--- mytime.py
+++ mytime.py
@@ -1,21 +1,21 @@
 from datetime import datetime, timedelta, timezone
 
-tz_game_server = timezone(timedelta(hours=9))
+tz_gmt_8 = timezone(timedelta(hours=-8))  # Change timezone offset to GMT-8
 
 def GetNowTimeHour():
-    return datetime.now(tz=tz_game_server).hour
+    return datetime.now(tz=tz_gmt_8).hour
 
 def GetNowTime():
-    return datetime.now(tz=tz_game_server)
+    return datetime.now(tz=tz_gmt_8)
 
 def GetFormattedNowTime():
-    return datetime.now(tz=tz_game_server).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.now(tz=tz_gmt_8).strftime('%Y-%m-%d %H:%M:%S')
 
 def GetTimeStamp():
-    return int(datetime.now(tz=tz_game_server).timestamp())
+    return int(datetime.now(tz=tz_gmt_8).timestamp())
 
 def TimeStampToString(timestamp):
-    return datetime.fromtimestamp(timestamp, tz=tz_game_server)
+    return datetime.fromtimestamp(timestamp, tz=tz_gmt_8)
 
 def GetNowTimeFileName():
-    return datetime.now(tz=tz_game_server).strftime('%Y/%m/%d.log')
+    return datetime.now(tz=tz_gmt_8).strftime('%Y/%m/%d.log')
