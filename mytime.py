b491b196ed0683edf2b83b8a1a4851eda5aa3442
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-21 01:22:54 -0600

    Update mytime.py

--- mytime.py
+++ mytime.py
@@ -1,21 +1,21 @@
 from datetime import datetime, timedelta, timezone
 
-tz_gmt_9 = timezone(timedelta(hours=9))  # Change timezone offset to GMT+9
+tz_utc_9 = timezone(timedelta(hours=9))  # Change timezone offset to UTC+9
 
 def GetNowTimeHour():
-    return datetime.now(tz=tz_gmt_9).hour
+    return datetime.now(tz=tz_utc_9).hour
 
 def GetNowTime():
-    return datetime.now(tz=tz_gmt_9)
+    return datetime.now(tz=tz_utc_9)
 
 def GetFormattedNowTime():
-    return datetime.now(tz=tz_gmt_9).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.now(tz=tz_utc_9).strftime('%Y-%m-%d %H:%M:%S')
 
 def GetTimeStamp():
-    return int(datetime.now(tz=tz_gmt_9).timestamp())
+    return int(datetime.now(tz=tz_utc_9).timestamp())
 
 def TimeStampToString(timestamp):
-    return datetime.fromtimestamp(timestamp, tz=tz_gmt_9)
+    return datetime.fromtimestamp(timestamp, tz=tz_utc_9)
 
 def GetNowTimeFileName():
-    return datetime.now(tz=tz_gmt_9).strftime('%Y/%m/%d.log')
+    return datetime.now(tz=tz_utc_9).strftime('%Y/%m/%d.log')
