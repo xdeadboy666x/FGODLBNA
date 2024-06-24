48d4a7cdb6a0c71a6c9fdfb8e8ff27e04e000ff1
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-23 18:58:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ secretKeyNums = len(secretKeys)
 
 def get_latest_verCode():
     try:
-        endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-VerCode-extractor-JP/JP/VerCode.json"
+        endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/JP/VerCode.json"
         response = requests.get(endpoint)
         response.raise_for_status()
         response_data = response.json()
