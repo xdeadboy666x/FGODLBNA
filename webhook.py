2f09c5ed38997d474115e6d430ad17af91858ec3
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-06-25 19:54:00 -0600

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -2,38 +2,51 @@ import main
 import requests
 import user
 import json
+from typing import Union
+
+# Define Dracula color palette
+dracula_colors = {
+    "purple": 0xBD93F9,
+    "pink": 0xFF79C6,
+    "blue": 0x6272A4,
+    "green": 0x50FA7B,
+    "yellow": 0xF1FA8C,
+    "orange": 0xFFB86C,
+}
 
 def topLogin(data: list) -> None:
     endpoint = main.webhook_discord_url
 
     rewards: user.Rewards = data[0]
     login: user.Login = data[1]
-    bonus: user.Bonus or str = data[2]
+    bonus: Union[user.Bonus, str] = data[2]
 
     with open('login.json', 'r', encoding='utf-8') as f:
         data22 = json.load(f)
 
-        name1 = data22['cache']['replaced']['userGame'][0]['name']
-        fpids1 = data22['cache']['replaced']['userGame'][0]['friendCode']
-    
+    name1 = data22['cache']['replaced']['userGame'][0]['name']
+    fpids1 = data22['cache']['replaced']['userGame'][0]['friendCode']
+
     messageBonus = ''
     nl = '\n'
 
-    if bonus != "No Bonus":
+    if isinstance(bonus, user.Bonus):
         messageBonus += f"__{bonus.message}__{nl}```{nl.join(bonus.items)}```"
 
         if bonus.bonus_name is not None:
             messageBonus += f"{nl}__{bonus.bonus_name}__{nl}{bonus.bonus_detail}{nl}```{nl.join(bonus.bonus_camp_items)}```"
 
         messageBonus += "\n"
+    elif isinstance(bonus, str) and bonus != "No Bonus":
+        messageBonus += f"{bonus}\n"
 
     jsonData = {
         "content": None,
         "embeds": [
             {
-                "title": "FGO Login System - " + main.fate_region,
-                "description": f"Details.\n\n{messageBonus}",
-                "color": 563455,
+                "title": f"Fate/Grand Order Login System - {main.fate_region}",
+                "description": f"Login success.\n\n{messageBonus}",
+                "color": dracula_colors["purple"],  # Dracula purple color
                 "fields": [
                     {"name": "Master Name", "value": f"{name1}", "inline": True},
                     {"name": "Friend Code", "value": f"{fpids1}", "inline": True},
@@ -44,13 +57,13 @@ def topLogin(data: list) -> None:
                     {"name": "Golden Fruit", "value": f"{rewards.goldenfruit}", "inline": True},
                     {"name": "Silver Fruit", "value": f"{rewards.silverfruit}", "inline": True},
                     {"name": "Bronze Fruit", "value": f"{rewards.bronzefruit}", "inline": True},
-                    {"name": "Bronzed Cobalt Fruit", "value": f"{rewards.bluebronzefruit}", "inline": True},
+                    {"name": "Bronze Cobalt Fruit", "value": f"{rewards.bluebronzefruit}", "inline": True},
                     {"name": "Bronze Sapling", "value": f"{rewards.bluebronzesapling}", "inline": True},
                     {"name": "Total Login Days", "value": f"{login.login_days} / {login.total_days}", "inline": True},
                     {"name": "Pure Prism", "value": f"{rewards.pureprism}", "inline": True},
                     {"name": "FP", "value": f"{login.total_fp}", "inline": True},
                     {"name": "Gained FP", "value": f"+{login.add_fp}", "inline": True},
-                    #{"name": "Current AP", "value": f"{login.remaining_ap}", "inline": True},
+                    # {"name": "Current AP", "value": f"{login.remaining_ap}", "inline": True},  # Uncomment if needed
                     {"name": "Holy Grail", "value": f"{rewards.holygrail}", "inline": True},
                 ],
                 "thumbnail": {
@@ -72,11 +85,11 @@ def shop(item: str, quantity: str) -> None:
         "content": None,
         "embeds": [
             {
-                "title": "FGO Auto Shop System - " + main.fate_region,
-                "description": f"Purchase successful.",
-                "color": 5814783,
+                "title": f"Bronze Cobalt Fruit Shop - {main.fate_region}",
+                "description": f"Successful exchange.",
+                "color": dracula_colors["pink"],  # Dracula pink color
                 "fields": [
-                    {"name": f"Shop", "value": f"Spent {40 * quantity} AP to buy {quantity}x {item}", "inline": False}
+                    {"name": f"Shop", "value": f"Spent {40 * int(quantity)} AP to purchase {quantity}x {item} (40 AP each)", "inline": False}
                 ],
                 "thumbnail": {
                     "url": "https://www.fate-go.jp/manga_fgo3/images/commnet_chara10.png"
@@ -115,11 +128,11 @@ def drawFP(servants, missions) -> None:
         "content": None,
         "embeds": [
             {
-                "title": "FGO Auto Summoning System - " + main.fate_region,
-                "description": f"Gacha results.\n\n{message_mission}",
-                "color": 5750876,
+                "title": f"Fate/Grand Order FP Summon System - {main.fate_region}",
+                "description": f"Completed daily free Friend Point summon. Displaying summon results.\n\n{message_mission}",
+                "color": dracula_colors["blue"],  # Dracula blue color
                 "fields": [
-                    {"name": "Friend Point Summon Pool", "value": f"{message_servant}", "inline": False}
+                    {"name": "FP Gacha results", "value": f"{message_servant}", "inline": False}
                 ],
                 "thumbnail": {
                     "url": "https://www.fate-go.jp/manga_fgo3/images/commnet_chara04_rv.png"
