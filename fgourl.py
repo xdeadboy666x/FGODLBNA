83d7247c334b6fb441490dd44498d817e0b6cc58
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-07-05 01:08:07 -0600

    Update fgourl.py

--- fgourl.py
+++ fgourl.py
@@ -6,73 +6,69 @@ import version
 import main
 import CatAndMouseGame
 
+# Disable insecure request warnings
 requests.urllib3.disable_warnings()
+
+# Function to retrieve user agent from environment variables
+def get_user_agent():
+    return os.environ.get('USER_AGENT_SECRET')
+
+# Global variables (consider avoiding global variables if possible)
 session = requests.Session()
 session.verify = False
+server_addr_ = 'https://game.fate-go.jp'
+github_token_ = ''  # Unused in provided snippet
+github_name_ = ''  # Unused in provided snippet
 
-# ===== Game's parameters =====
+# Game's parameters
 app_ver_ = ''
 data_ver_ = 0
 date_ver_ = 0
 ver_code_ = ''
 asset_bundle_folder_ = ''
 data_server_folder_crc_ = 0
-server_addr_ = 'https://game.fate-go.jp'
-github_token_ = ''
-github_name_ = ''
+user_agent_ = get_user_agent()
 
+# HTTP headers
+httpheader = {
+    'Accept-Encoding': 'gzip, identity',
+    'User-Agent': user_agent_ if user_agent_ else '',  # Handle potential None value
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Connection': 'Keep-Alive, TE',
+    'TE': 'identity',
+    'X-Unity-Version': "2022.3.28f1"
+}
 
-# ==== User Info ====
 def set_latest_assets():
-    global app_ver_, data_ver_, date_ver_, asset_bundle_folder_, data_server_folder_crc_, ver_code_, server_addr_
+    global app_ver_, data_ver_, date_ver_, asset_bundle_folder_, data_server_folder_crc_, server_addr_
 
-    region = main.fate_region
-
-    # Set Game Server Depends of region
+    region = main.fate_region  # Assuming fate_region is defined in main module
 
     if region == "NA":
         server_addr_ = "https://game.fate-go.us"
 
-    # Get Latest Version of the data!
-    version_str = version.get_version(region)
-    response = requests.get(
-        server_addr_ + '/gamedata/top?appVer=' + version_str).text
+    # Get Latest Version of the data
+    version_str = version.get_version(region)  # Ensure version module has this function
+    response = requests.get(f'{server_addr_}/gamedata/top?appVer={version_str}').text
     response_data = json.loads(response)["response"][0]["success"]
 
     # Set AppVer, DataVer, DateVer
     app_ver_ = version_str
     data_ver_ = response_data['dataVer']
     date_ver_ = response_data['dateVer']
-    ver_code_ = main.get_latest_verCode()
 
-    # Use Asset Bundle Extractor to get Folder Name
+    # Get assetbundle and extract folder name
     assetbundle = CatAndMouseGame.getAssetBundle(response_data['assetbundle'])
     get_folder_data(assetbundle)
-
 
 def get_folder_data(assetbundle):
     global asset_bundle_folder_, data_server_folder_crc_
 
     asset_bundle_folder_ = assetbundle['folderName']
-    data_server_folder_crc_ = binascii.crc32(
-        assetbundle['folderName'].encode('utf8'))
-
-# ===== End =====
-
-user_agent_ = os.environ.get('USER_AGENT_SECRET')
-
-httpheader = {
-    'User-Agent': user_agent_,
-    'Accept-Encoding': "deflate, gzip",
-    'Content-Type': "application/x-www-form-urlencoded",
-    'X-Unity-Version': "2020.3.34f1"
-
-}
-
+    data_server_folder_crc_ = binascii.crc32(assetbundle['folderName'].encode('utf8'))
 
 def NewSession():
     return requests.Session()
-
 
 def PostReq(s, url, data):
     res = s.post(url, data=data, headers=httpheader, verify=False).json()
