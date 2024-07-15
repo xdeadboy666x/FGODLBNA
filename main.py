bc5977bf8b12194eaf730326a8743b790eaef24d
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-07-14 23:23:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import time
+import json
 from datetime import datetime
 from croniter import croniter
 import fgourl
@@ -15,55 +16,51 @@ secretKeys = os.environ['secretKeys'].split(',')
 fate_region = os.environ['fateRegion']
 webhook_discord_url = os.environ['webhookDiscord']
 blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
+
 UA = os.environ['UserAgent']
 
 if UA:
     fgourl.user_agent_ = UA
 
-# Logger setup
-logger = logging.getLogger("Fate/Grand Order Login Manager")
-coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s', logger=logger)
+userNums = len(userIds)
+authKeyNums = len(authKeys)
+secretKeyNums = len(secretKeys)
+
+logger = logging.getLogger("FGO Daily Login")
+coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+
 
 def check_blue_apple_cron(instance):
-    """Check if the blue apple cron schedule has reached and perform the exchange if necessary."""
     if blue_apple_cron:
         cron = croniter(blue_apple_cron)
         next_date = cron.get_next(datetime)
         current_date = datetime.now()
-        
+
         if current_date >= next_date:
-            logger.info('AP Available')
+            logger.info('Exchanging Blue Fruit!')
             instance.buyBlueApple(1)
             time.sleep(2)
 
+
 def get_latest_verCode():
-    """Fetch the latest version code from the given endpoint."""
     endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
-    try:
-        response = requests.get(endpoint)
-        response.raise_for_status()
-        response_data = response.json()
-        return response_data['verCode']
-    except requests.RequestException as e:
-        logger.error(f"Failed to fetch the latest vercode: {e}")
-        raise
+
+    response = requests.get(endpoint).text
+    response_data = json.loads(response)
+
+    return response_data['verCode']
+
 
 def main():
-    """Main function to handle the daily login process for FGO."""
-    if len(userIds) == len(authKeys) == len(secretKeys):
-        logger.info('Fetch gamedata assets')
-        try:
-            fgourl.set_latest_assets()
-        except Exception as ex:
-            logger.error(f"Failed to set latest assets: {ex}")
-            return
+    if userNums == authKeyNums and userNums == secretKeyNums:
+        logger.info('Fetching Game Data')
+        fgourl.set_latest_assets()
 
-        for i in range(len(userIds)):
+        for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                
-                logger.info('Logging in....')
+                logger.info('Logging in...')
                 instance.topLogin()
                 time.sleep(2)
                 instance.topHome()
@@ -73,7 +70,7 @@ def main():
                 time.sleep(2)
                 
                 check_blue_apple_cron(instance)
-                
+
                 logger.info('Summoning with FP!')
                 try:
                     instance.FPsummon()
@@ -81,18 +78,21 @@ def main():
                 except Exception as ex:
                     logger.error(f"FP summoning failed: {ex}")
 
-                logger.info('AP and Bronze Sapling available!')
+                check_blue_apple_cron(instance)
+                logger.info('Exchanging Blue Fruit!')
                 try:
                     for _ in range(4):  # Exchanging once in check_blue_apple_cron and three times here
+                    instance.buyBlueApple(1)
+                    time.sleep(2)
+                    for _ in range(3):
                         instance.buyBlueApple(1)
                         time.sleep(2)
                 except Exception as ex:
-                    logger.error(f"No AP: {ex}")
-                
+                    logger.error(ex)
+
             except Exception as ex:
-                logger.error(f"Error during user operation for user {userIds[i]}: {ex}")
-    else:
-        logger.error("Mismatch in the number of userIds, authKeys, and secretKeys")
+                logger.error(ex)
+
 
 if __name__ == "__main__":
     main()
