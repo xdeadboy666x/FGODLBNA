ea47beab9b3aefce505d75ff877dda3250514d02
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-07-24 14:12:14 -0600

    Update user.py

--- user.py
+++ user.py
@@ -26,6 +26,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from urllib.parse import quote_plus
 from libs.GetSubGachaId import GetGachaSubIdFP
+import rsa
 
 class ParameterBuilder:
     def __init__(self, uid: str, auth_key: str, secret_key: str):
