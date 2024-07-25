947dfac7ee92b8d029f367df456f53f14a757bcc
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-07-25 07:49:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ import user
 import coloredlogs
 import logging
 
-# Environment variables
-userIds = os.environ.get('userIds', '').split(',')
-authKeys = os.environ.get('authKeys', '').split(',')
-secretKeys = os.environ.get('secretKeys', '').split(',')
-webhook_discord_url = os.environ.get('webhookDiscord')
+userIds = os.environ['userIds'].split(',')
+authKeys = os.environ['authKeys'].split(',')
+secretKeys = os.environ['secretKeys'].split(',')
+webhook_discord_url = os.environ['webhookDiscord']
 device_info = os.environ.get('DEVICE_INFO_SECRET')
 user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
 fate_region = os.environ['fateRegion']
 
-# Logger setup
+userNums = len(userIds)
+authKeyNums = len(authKeys)
+secretKeyNums = len(secretKeys)
+
 logger = logging.getLogger("FGO Daily Login")
 coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
@@ -24,20 +26,19 @@ def get_latest_verCode():
     endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
+
     return response_data['verCode']
 
-def main():
-    userNums = len(userIds)
-    authKeyNums = len(authKeys)
-    secretKeyNums = len(secretKeys)
 
+
+def main():
     if userNums == authKeyNums and userNums == secretKeyNums:
         fgourl.set_latest_assets()
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info("========================================\n[+] Logging in to account\n========================================")
+                logger.info(f"\n ======================================== \n [+] 登录账号 \n ======================================== " )
 
                 time.sleep(1)
                 instance.topLogin_s()
@@ -53,8 +54,9 @@ def main():
                 time.sleep(1)
                 instance.FPsummon()
 
+
             except Exception as ex:
-                logger.error(f"Error for user {userIds[i]}: {ex}")
+                logger.error(ex)
 
 if __name__ == "__main__":
     main()
