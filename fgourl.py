b0e096b11d7ddd46597407f3a3f37a92df9dd2dd
alienchristxv3 <daeryk@gmail.com>
2024-08-05 14:52:36 +0000

    Update fgourl.py

--- fgourl.py
+++ fgourl.py
@@ -66,10 +66,8 @@ user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
 
 httpheader = {
     'User-Agent': user_agent_2,
-    'Accept-Encoding': "deflate, gzip",
-    'Content-Type': "application/x-www-form-urlencoded",
-    'X-Unity-Version': "2022.3.28f1"
-
+    "Accept-Encoding": "gzip, identity",
+    "Content-Type": "application/x-www-form-urlencoded"
 }
 
 
