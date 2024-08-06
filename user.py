59fa04da380764aa3c3861cd4614ceeaa02e5279
alienchristxv3 <daeryk@gmail.com>
2024-08-06 06:26:23 +0000

    Update user.py

--- user.py
+++ user.py
@@ -25,7 +25,7 @@ from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from urllib.parse import quote_plus
-from libs.GetSubGachaId import GetGachaSubIdFP(region)
+from libs.GetSubGachaId import GetGachaSubIdFP
 
 class ParameterBuilder:
     def __init__(self, uid: str, auth_key: str, secret_key: str):
