17bc50c5ee6884532786625f109f2473f6997741
alienchristxv3 <daeryk@gmail.com>
2024-08-06 16:26:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import os
 import requests
 import time
 import json
-from datetime import datetime
-from croniter import croniter
 import fgourl
 import user
 import coloredlogs
 import logging
 
+# Environment variables
 userIds = os.environ['userIds'].split(',')
 authKeys = os.environ['authKeys'].split(',')
 secretKeys = os.environ['secretKeys'].split(',')
@@ -17,36 +16,35 @@ device_info = os.environ.get('DEVICE_INFO_SECRET')
 user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
 fate_region = 'NA'
 
+# Counts of users, auth keys, and secret keys
 userNums = len(userIds)
 authKeyNums = len(authKeys)
 secretKeyNums = len(secretKeys)
 
-logger = logging.getLogger("FGO Daily Login")
+# Logger setup
+logger = logging.getLogger("Fate/Grand Order Login Manager")
 coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 def get_latest_verCode():
-    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
+    endpoint = f'https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/{fate_region}/VerCode.json'
     response = requests.get(endpoint).text
     response_data = json.loads(response)
-
     return response_data['verCode']
     
 def get_latest_appver():
-    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
+    endpoint = f'https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/{fate_region}/VerCode.json'
     response = requests.get(endpoint).text
     response_data = json.loads(response)
-
     return response_data['appVer']
-
 
 def main():
     if userNums == authKeyNums and userNums == secretKeyNums:
         fgourl.set_latest_assets()
         for i in range(userNums):
             try:
-                instance = user.user(userIds[i], authKeys[i], secretKeys[i])
+                instance = user.User(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info(f"\n ======================================== \n [+] Signing in \n ======================================== " )
+                logger.info("\n ======================================== \n [+] Signing in \n ======================================== ")
 
                 time.sleep(1)
                 instance.topLogin_s()
@@ -61,7 +59,6 @@ def main():
                 instance.lq003()
                 time.sleep(1)
                 instance.drawFP()
-
 
             except Exception as ex:
                 logger.error(ex)
