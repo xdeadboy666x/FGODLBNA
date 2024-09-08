7fedd0c8822e906a5c41b6280d7aef4b1c58f7b6
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-08 04:30:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,13 @@ import user
 import coloredlogs
 import logging
 
-# Enviroments Variables
 userIds = os.environ['userIds'].split(',')
 authKeys = os.environ['authKeys'].split(',')
 secretKeys = os.environ['secretKeys'].split(',')
-fate_region = os.environ['fateRegion']
 webhook_discord_url = os.environ['webhookDiscord']
-blue_apple_cron = os.environ.get("MAKE_BLUE_APPLE")
-idempotency_key_signature = os.environ.get('IDEMPOTENCY_KEY_SIGNATURE_SECRET')
-
-
-UA = os.environ['UserAgent']
-
-if UA:
-    fgourl.user_agent_ = UA
+device_info = os.environ.get('DEVICE_INFO_SECRET')
+user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
+fate_region = 'NA'
 
 userNums = len(userIds)
 authKeyNums = len(authKeys)
@@ -30,18 +23,12 @@ logger = logging.getLogger("FGO Daily Login")
 coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 def get_latest_verCode():
-    endpoint = ""
-
-    if fate_region == "NA":
-        endpoint += "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
-    else:
-        endpoint += "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/JP/VerCode.json"
-
+    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
 
     return response_data['verCode']
-    
+
 def get_latest_appver():
     endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
     response = requests.get(endpoint).text
@@ -49,41 +36,33 @@ def get_latest_appver():
 
     return response_data['appVer']
 
+
 def main():
     if userNums == authKeyNums and userNums == secretKeyNums:
-        logger.info('Getting Lastest Assets Info')
         fgourl.set_latest_assets()
-
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info('Signing in!')
-                instance.topLogin()
+                logger.info(f"\n ======================================== \n [+] Signing \n ======================================== " )
+
+                time.sleep(1)
+                instance.topLogin_s()
                 time.sleep(2)
                 instance.topHome()
                 time.sleep(2)
-                instance.buyBlueApple()
                 instance.lq001()
                 instance.lq002()
+                time.sleep(2)
+                instance.buyBlueApple()
                 time.sleep(1)
                 instance.lq003()
                 time.sleep(1)
-                instance.zc15()
-                
-                #instance.zc16()
-                #instance.zc25() #导出账号功能
-                
-                time.sleep(1)
                 instance.drawFP()
-                #for _ in range(100): 
-                      #instance.drawS()
-                      #instance.drawFF()
-                      #time.sleep(1)
-                    
+
+
             except Exception as ex:
                 logger.error(ex)
-
 
 if __name__ == "__main__":
     main()
