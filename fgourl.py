1e4b1506366c80a7ac2f4db062b03349ebf206ea
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-08 03:59:08 -0600

    Update fgourl.py

--- fgourl.py
+++ fgourl.py
@@ -1,10 +1,9 @@
-# encodng_mode: utf-8
 import json
 import binascii
 import requests
+import version
 import main
 import CatAndMouseGame
-import os
 
 requests.urllib3.disable_warnings()
 session = requests.Session()
@@ -17,8 +16,10 @@ date_ver_ = 0
 ver_code_ = ''
 asset_bundle_folder_ = ''
 data_server_folder_crc_ = 0
-server_addr_ = 'https://game.fate-go.us'
-
+server_addr_ = 'https://game.fate-go.jp'
+github_token_ = ''
+github_name_ = ''
+user_agent_ = 'Dalvik/2.1.0 (Linux; U; Android 10; MAR-LX3Bm Build/HUAWEIMAR-L03B)"
 
 # ==== User Info ====
 def set_latest_assets():
@@ -28,13 +29,11 @@ def set_latest_assets():
 
     # Set Game Server Depends of region
 
-    if region == "JP":
-        server_addr_ = "https://game.fate-go.jp"
+    if region == "NA":
+        server_addr_ = "https://game.fate-go.us"
 
     # Get Latest Version of the data!
     version_str = main.get_latest_appver()
-    #main.logger.info(f"vv{version_str}")
-
     response = requests.get(
         server_addr_ + '/gamedata/top?appVer=' + version_str).text
     response_data = json.loads(response)["response"][0]["success"]
@@ -44,8 +43,6 @@ def set_latest_assets():
     data_ver_ = response_data['dataVer']
     date_ver_ = response_data['dateVer']
     ver_code_ = main.get_latest_verCode()
-
-    #main.logger.info(f"ver{ver_code_}")
 
     # Use Asset Bundle Extractor to get Folder Name
     assetbundle = CatAndMouseGame.getAssetBundle(response_data['assetbundle'])
@@ -61,13 +58,12 @@ def get_folder_data(assetbundle):
 
 # ===== End =====
 
-user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
 
 httpheader = {
-    'User-Agent': user_agent_2,
-    'Accept-Encoding': "deflate, gzip",
-    'Content-Type': "application/x-www-form-urlencoded"
-
+    'Accept-Encoding': 'deflate, gzip',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'User-Agent': user_agent_,
+    'X-Unity-Version': "2022.3.28f1"
 }
 
 
