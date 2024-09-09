f5c022075697516ae0557282b264fa8b4434f218
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-09 04:03:09 -0600

    Update mytime.py

--- mytime.py
+++ mytime.py
@@ -1,8 +1,8 @@
 import math
 from datetime import datetime, timedelta, timezone
 
-# Set timezone to UTC+9
-tz_utc_9 = timezone(timedelta(hours=9))
+# Set timezone to UTC+4
+tz_utc_4 = timezone(timedelta(hours=4))
 
 class MyTime:
     @classmethod
@@ -11,11 +11,11 @@ class MyTime:
 
     @classmethod
     def is_Free_FP_draw_available(cls, last_free_draw_timestamp_utc: int):
-        JST = timezone(timedelta(hours=+9))
-        # get last free draw time in GMT+0 and convert to GMT+9 to the next day
+        UTC_PLUS_4 = timezone(timedelta(hours=+4))
+        # get last free draw time in GMT+0 and convert to GMT+4 to the next day
         dt_utc = datetime.fromtimestamp(last_free_draw_timestamp_utc, timezone.utc)
-        dt_japan = dt_utc.astimezone(JST)
-        next_midnight = datetime(dt_japan.year, dt_japan.month, dt_japan.day, tzinfo=JST) + timedelta(days=1)
+        dt_custom = dt_utc.astimezone(UTC_PLUS_4)
+        next_midnight = datetime(dt_custom.year, dt_custom.month, dt_custom.day, tzinfo=UTC_PLUS_4) + timedelta(days=1)
 
         # Convert next_midnight back to UTC to compare with the current time
         next_midnight_utc = next_midnight.astimezone(timezone.utc)
@@ -28,21 +28,21 @@ class MyTime:
     def get_used_act_ammount(cls, full_recover_at: int):
         return max(0, math.ceil((full_recover_at - cls.get_timestamp()) / 300))
 
-# Existing functions with timezone set to UTC+9
+# Updated functions with timezone set to UTC+4
 def GetNowTimeHour():
-    return datetime.now(tz=tz_utc_9).hour
+    return datetime.now(tz=tz_utc_4).hour
 
 def GetNowTime():
-    return datetime.now(tz=tz_utc_9)
+    return datetime.now(tz=tz_utc_4)
 
 def GetFormattedNowTime():
-    return datetime.now(tz=tz_utc_9).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.now(tz=tz_utc_4).strftime('%Y-%m-%d %H:%M:%S')
 
 def GetTimeStamp():
-    return int(datetime.now(tz=tz_utc_9).timestamp())
+    return int(datetime.now(tz=tz_utc_4).timestamp())
 
 def TimeStampToString(timestamp):
-    return datetime.fromtimestamp(timestamp, tz=tz_utc_9).strftime('%Y-%m-%d %H:%M:%S')
+    return datetime.fromtimestamp(timestamp, tz=tz_utc_4).strftime('%Y-%m-%d %H:%M:%S')
 
 def GetNowTimeFileName():
-    return datetime.now(tz=tz_utc_9).strftime('%Y/%m/%d.log')
+    return datetime.now(tz=tz_utc_4).strftime('%Y/%m/%d.log')
