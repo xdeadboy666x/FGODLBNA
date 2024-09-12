e45677cd55e20d008b6c7ef402747b5b84e2418f
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-12 01:46:34 -0600

    Update GetSubGachaId.py

--- libs/GetSubGachaId.py
+++ libs/GetSubGachaId.py
@@ -5,7 +5,7 @@ from mytime import GetTimeStamp
 
 # Get Friend Summon Gacha Sub Id
 def GetGachaSubIdFP(region):
-    response = requests.get(f"https://git.atlasacademy.io/atlasacademy/fgo-game-data/raw/branch/NA/master/mstGachaSub.json");
+    response = requests.get(f"https://git.atlasacademy.io/atlasacademy/fgo-game-data/raw/branch/{region}/master/mstGachaSub.json");
     gachaList = json.loads(response.text)
     timeNow = GetTimeStamp()
     priority = 0
@@ -19,4 +19,4 @@ def GetGachaSubIdFP(region):
             if(p > priority):
                 priority = p
                 goodGacha = gacha
-    return str(goodGacha["id"])i
+    return str(goodGacha["id"])
