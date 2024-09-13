f52587cec6c8379fdaf452d5f233003dd2da9d92
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-12 18:54:31 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,66 +2,43 @@ import os
 import requests
 import time
 import json
-from datetime import datetime
-from croniter import croniter
 import fgourl
 import user
 import coloredlogs
 import logging
 
-# Setup logger
-logger = logging.getLogger("FGO Daily Login")
-coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
-
-# Check and retrieve environment variables
-def get_env_var(var_name):
-    value = os.environ.get(var_name)
-    if not value:
-        logger.error(f"Environment variable {var_name} is not set.")
-        raise EnvironmentError(f"Environment variable {var_name} is not set.")
-    return value.split(',')
-
-try:
-    userIds = get_env_var('userIds')
-    authKeys = get_env_var('authKeys')
-    secretKeys = get_env_var('secretKeys')
-    webhook_discord_url = os.environ.get('webhookDiscord')
-    device_info = os.environ.get('DEVICE_INFO_SECRET')
-    user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
-except EnvironmentError as e:
-    logger.critical(e)
-    exit(1)
-
-fate_region = 'NA'
+userIds = os.environ['userIds'].split(',')
+authKeys = os.environ['authKeys'].split(',')
+secretKeys = os.environ['secretKeys'].split(',')
+fate_region = os.environ['fateRegion']
+webhook_discord_url = os.environ['webhookDiscord']
+device_info = os.environ.get('DEVICE_INFO_SECRET')
+user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
 
 userNums = len(userIds)
 authKeyNums = len(authKeys)
 secretKeyNums = len(secretKeys)
 
+logger = logging.getLogger("FGO Daily Login")
+coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+
 def get_latest_verCode():
-    try:
-        endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
-        response = requests.get(endpoint)
-        response.raise_for_status()
-        response_data = response.json()
-        return response_data['verCode']
-    except requests.RequestException as e:
-        logger.error(f"Failed to get the latest verCode: {e}")
-        raise
+    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
+    response = requests.get(endpoint).text
+    response_data = json.loads(response)
+
+    return response_data['verCode']
+
+
 
 def main():
-    if userNums == authKeyNums == secretKeyNums:
-        try:
-            fgourl.set_latest_assets()
-        except Exception as ex:
-            logger.error(f"Failed to set latest assets: {ex}")
-            return
-
+    if userNums == authKeyNums and userNums == secretKeyNums:
+        fgourl.set_latest_assets()
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info(f"Logging in to account {i+1}/{userNums}")
+                logger.info(f"\n ======================================== \n [+] Signing in \n ======================================== " )
 
                 time.sleep(1)
                 instance.topLogin_s()
@@ -76,11 +53,10 @@ def main():
                 instance.lq003()
                 time.sleep(1)
                 instance.drawFP()
+
+
             except Exception as ex:
-                logger.error(f"Error with account {i+1}/{userNums}: {ex}")
-    else:
-        logger.error("The number of user IDs, auth keys, and secret keys do not match.")
-        raise ValueError("Mismatched user IDs, auth keys, and secret keys count.")
+                logger.error(ex)
 
 if __name__ == "__main__":
     main()
