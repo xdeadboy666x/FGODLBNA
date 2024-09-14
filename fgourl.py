238ae1144c36bc802f83b19fa870a86dab2c855c
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-14 00:58:37 -0600

    Update fgourl.py

--- fgourl.py
+++ fgourl.py
@@ -10,15 +10,15 @@ session = requests.Session()
 session.verify = False
 
 # ===== Game's parameters =====
-app_ver_ = ''
+app_ver_ = ""
 data_ver_ = 0
 date_ver_ = 0
-ver_code_ = ''
-asset_bundle_folder_ = ''
+ver_code_ = ""
+asset_bundle_folder_ = ""
 data_server_folder_crc_ = 0
-server_addr_ = 'https://game.fate-go.jp'
-github_token_ = ''
-github_name_ = ''
+server_addr_ = "https://game.fate-go.jp"
+github_token_ = ""
+github_name_ = ""
 
 
 # ==== User Info ====
@@ -34,42 +34,40 @@ def set_latest_assets():
 
     # Get Latest Version of the data!
     version_str = main.get_latest_appver()
-    #main.logger.info(f"vv{version_str}")
+    # main.logger.info(f"vv{version_str}")
 
-    response = requests.get(
-        server_addr_ + '/gamedata/top?appVer=' + version_str).text
+    response = requests.get(server_addr_ + "/gamedata/top?appVer=" + version_str).text
     response_data = json.loads(response)["response"][0]["success"]
 
     # Set AppVer, DataVer, DateVer
     app_ver_ = version_str
-    data_ver_ = response_data['dataVer']
-    date_ver_ = response_data['dateVer']
+    data_ver_ = response_data["dataVer"]
+    date_ver_ = response_data["dateVer"]
     ver_code_ = main.get_latest_verCode()
 
-    #main.logger.info(f"ver{ver_code_}")
+    # main.logger.info(f"ver{ver_code_}")
 
     # Use Asset Bundle Extractor to get Folder Name
-    assetbundle = CatAndMouseGame.getAssetBundle(response_data['assetbundle'])
+    assetbundle = CatAndMouseGame.getAssetBundle(response_data["assetbundle"])
     get_folder_data(assetbundle)
 
 
 def get_folder_data(assetbundle):
     global asset_bundle_folder_, data_server_folder_crc_
 
-    asset_bundle_folder_ = assetbundle['folderName']
-    data_server_folder_crc_ = binascii.crc32(
-        assetbundle['folderName'].encode('utf8'))
+    asset_bundle_folder_ = assetbundle["folderName"]
+    data_server_folder_crc_ = binascii.crc32(assetbundle["folderName"].encode("utf8"))
+
 
 # ===== End =====
 
-user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
+user_agent_2 = os.environ.get("USER_AGENT_SECRET_2")
 
 httpheader = {
-    'User-Agent': user_agent_2,
-    'Accept-Encoding': "deflate, gzip",
-    'Content-Type': "application/x-www-form-urlencoded",
-    'X-Unity-Version': "2022.3.28f1"
-
+    "User-Agent": user_agent_2,
+    "Accept-Encoding": "deflate, gzip",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "X-Unity-Version": "2022.3.28f1",
 }
 
 
@@ -79,11 +77,11 @@ def NewSession():
 
 def PostReq(s, url, data):
     res = s.post(url, data=data, headers=httpheader, verify=False).json()
-    res_code = res['response'][0]['resCode']
+    res_code = res["response"][0]["resCode"]
 
-    if res_code != '00':
-        detail = res['response'][0]['fail']['detail']
-        message = f'[ErrorCode: {res_code}]\n{detail}'
+    if res_code != "00":
+        detail = res["response"][0]["fail"]["detail"]
+        message = f"[ErrorCode: {res_code}]\n{detail}"
         raise Exception(message)
 
     return res
