b0d55431c7a2b496e1c5f3d38dd912dff0032f1b
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-14 01:02:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,36 +6,36 @@ import fgourl
 import user
 import coloredlogs
 import logging
-from pathlib import Path
-from typing import Any
-import orjson
 
 # Environment variables
-userIds = os.environ['userIds'].split(',')
-authKeys = os.environ['authKeys'].split(',')
-secretKeys = os.environ['secretKeys'].split(',')
-webhook_discord_url = os.environ['webhookDiscord']
-device_info = os.environ.get('DEVICE_INFO_SECRET')
-user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
-fate_region = 'NA'
+userIds = os.environ["userIds"].split(",")
+authKeys = os.environ["authKeys"].split(",")
+secretKeys = os.environ["secretKeys"].split(",")
+webhook_discord_url = os.environ["webhookDiscord"]
+device_info = os.environ.get("DEVICE_INFO_SECRET")
+user_agent_2 = os.environ.get("USER_AGENT_SECRET_2")
+fate_region = "NA"
 
 # Initialize logger
 logger = logging.getLogger("FGO Daily Login")
-coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+coloredlogs.install(fmt="%(asctime)s %(name)s %(levelname)s %(message)s")
+
 
 def get_latest_verCode():
     """Fetch the latest version code from the VerCode API."""
     endpoint = f"https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/{fate_region}/VerCode.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
-    return response_data['verCode']
+    return response_data["verCode"]
+
 
 def get_latest_appver():
     """Fetch the latest app version from the VerCode API."""
     endpoint = f"https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/{fate_region}/VerCode.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
-    return response_data['appVer']
+    return response_data["appVer"]
+
 
 def main():
     if len(userIds) == len(authKeys) == len(secretKeys):
@@ -44,7 +44,9 @@ def main():
             try:
                 # Create user instance and perform operations
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
-                logger.info(f"\n ======================================== \n [+] Logging in account: {userIds[i]} \n ======================================== ")
+                logger.info(
+                    f"\n ======================================== \n [+] Logging in account: {userIds[i]} \n ======================================== "
+                )
 
                 # Call instance methods with delays
                 time.sleep(3)
@@ -64,6 +66,7 @@ def main():
                 logger.error(f"Network error: {ex}")
             except Exception as ex:
                 logger.error(f"An error occurred: {ex}")
+
 
 if __name__ == "__main__":
     main()
