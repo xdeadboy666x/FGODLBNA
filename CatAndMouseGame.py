67edc17440bcf2aff2de321c308500747f54d5e6
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-14 00:57:21 -0600

    Update CatAndMouseGame.py

--- CatAndMouseGame.py
+++ CatAndMouseGame.py
@@ -7,16 +7,11 @@ import msgpack
 def getAssetBundle(assetbundle):
     data = base64.b64decode(assetbundle)
 
-    key = b'nn33CYId2J1ggv0bYDMbYuZ60m4GZt5P'
+    key = b"nn33CYId2J1ggv0bYDMbYuZ60m4GZt5P"
     iv = data[:32]
     array = data[32:]
 
-    cipher = py3rijndael.RijndaelCbc(
-        key,
-        iv,
-        py3rijndael.paddings.Pkcs7Padding(16),
-        32
-    )
+    cipher = py3rijndael.RijndaelCbc(key, iv, py3rijndael.paddings.Pkcs7Padding(16), 32)
 
     data = cipher.decrypt(array)
     gzip_data = gzip.decompress(data)
