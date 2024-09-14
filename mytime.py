8e5e0a4b68866269e681ead38b2c0b2cb99ef5ee
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-14 03:07:17 -0600

    Update mytime.py

--- mytime.py
+++ mytime.py
@@ -1,5 +1,6 @@
-import math
 from datetime import datetime, timedelta, timezone
+
+tz_utc_8 = timezone(timedelta(hours=8))
 
 
 def GetNowTimeHour():
@@ -11,7 +12,7 @@ def GetNowTime():
 
 
 def GetFormattedNowTime():
-    return datetime.now(tz=tz_utc_8).strftime("%Y-%m-%d %H:%M:%S")
+    return datetime.now(tz=tz_utc_8).strftime('%Y-%m-%d %H:%M:%S')
 
 
 def GetTimeStamp():
@@ -23,23 +24,4 @@ def TimeStampToString(timestamp):
 
 
 def GetNowTimeFileName():
-    return datetime.now(tz=tz_utc_8).strftime("%Y/%m/%d.log")
-
-
-def get_time_stamp():
-    return str(int(time.time()))
-
-
-def get_asset_bundle(assetbundle):
-    data = base64.b64decode(assetbundle)
-    key = b"nn33CYId2J1ggv0bYDMbYuZ60m4GZt5P"  # NA key
-    if REGION == "JP":
-        key = b"W0Juh4cFJSYPkebJB9WpswNF51oa6Gm7"  # JP key
-    iv = data[:32]
-    array = data[32:]
-    cipher = py3rijndael.RijndaelCbc(key, iv, py3rijndael.paddings.Pkcs7Padding(16), 32)
-    data = cipher.decrypt(array)
-    gzip_data = gzip.decompress(data)
-    data_unpacked = msgpack.unpackb(gzip_data)
-
-    return data_unpacked
+    return datetime.now(tz=tz_utc_8).strftime('%Y/%m/%d.log')
