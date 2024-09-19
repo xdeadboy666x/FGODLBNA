2228d4bb5e3100e8718354d5dd88c0382dc96173
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-09-18 21:30:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,34 +7,36 @@ import user
 import coloredlogs
 import logging
 
-userIds = os.environ['userIds'].split(',')
-authKeys = os.environ['authKeys'].split(',')
-secretKeys = os.environ['secretKeys'].split(',')
-webhook_discord_url = os.environ['webhookDiscord']
-device_info = os.environ.get('DEVICE_INFO_SECRET')
-user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
-fate_region = 'NA'
+userIds = os.environ["userIds"].split(",")
+authKeys = os.environ["authKeys"].split(",")
+secretKeys = os.environ["secretKeys"].split(",")
+webhook_discord_url = os.environ["webhookDiscord"]
+device_info = os.environ.get("DEVICE_INFO_SECRET")
+user_agent_2 = os.environ.get("USER_AGENT_SECRET_2")
+fate_region = "NA"
 
 userNums = len(userIds)
 authKeyNums = len(authKeys)
 secretKeyNums = len(secretKeys)
 
 logger = logging.getLogger("FGO Daily Login")
-coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+coloredlogs.install(fmt="%(asctime)s %(name)s %(levelname)s %(message)s")
+
 
 def get_latest_verCode():
     endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
 
-    return response_data['verCode']
+    return response_data["verCode"]
+
 
 def get_latest_appver():
     endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
 
-    return response_data['appVer']
+    return response_data["appVer"]
 
 
 def main():
@@ -44,7 +46,9 @@ def main():
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
                 time.sleep(3)
-                logger.info(f"\n ======================================== \n [+]Signing in... \n ======================================== " )
+                logger.info(
+                    f"\n ======================================== \n [+]Signing in... \n ======================================== "
+                )
 
                 time.sleep(1)
                 instance.topLogin_s()
@@ -60,9 +64,9 @@ def main():
                 time.sleep(1)
                 instance.drawFP()
 
-
             except Exception as ex:
                 logger.error(ex)
+
 
 if __name__ == "__main__":
     main()
