00940cd4869c83efc63e238f262a09fa1dd01fd8
xdeadboy666x <154488832+xdeadboy666x@users.noreply.github.com>
2024-12-09 03:11:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,35 +7,36 @@ import user
 import coloredlogs
 import logging
 
-userIds = os.environ['userIds'].split(',')
-authKeys = os.environ['authKeys'].split(',')
-secretKeys = os.environ['secretKeys'].split(',')
-webhook_discord_url = os.environ['webhookDiscord']
-device_info = os.environ.get('DEVICE_INFO_SECRET')
-appCheck = os.environ.get('appCheckErrorMessage_SECRET')
-user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
-fate_region = 'JP'
+userIds = os.environ["userIds"].split(",")
+authKeys = os.environ["authKeys"].split(",")
+secretKeys = os.environ["secretKeys"].split(",")
+webhook_discord_url = os.environ["webhookDiscord"]
+device_info = os.environ.get("DEVICE_INFO_SECRET")
+user_agent_2 = os.environ.get("USER_AGENT_SECRET_2")
+fate_region = "NA"
 
 userNums = len(userIds)
 authKeyNums = len(authKeys)
 secretKeyNums = len(secretKeys)
 
 logger = logging.getLogger("FGO Daily Login")
-coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+coloredlogs.install(fmt="%(asctime)s %(name)s %(levelname)s %(message)s")
+
 
 def get_latest_verCode():
-    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/refs/heads/JP/VerCode.json"
+    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
 
-    return response_data['verCode']
-    
+    return response_data["verCode"]
+
+
 def get_latest_appver():
-    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/refs/heads/JP/VerCode.json"
+    endpoint = "https://raw.githubusercontent.com/xdeadboy666x/FGO-JP-NA-VerCode-Extractor/NA/VerCode.json"
     response = requests.get(endpoint).text
     response_data = json.loads(response)
 
-    return response_data['appVer']
+    return response_data["appVer"]
 
 
 def main():
@@ -44,29 +45,28 @@ def main():
         for i in range(userNums):
             try:
                 instance = user.user(userIds[i], authKeys[i], secretKeys[i])
+                time.sleep(3)
+                logger.info(
+                    f"\n ======================================== \n [+]Signing in... \n ======================================== "
+                )
+
                 time.sleep(1)
-                logger.info(f"\n {'=' * 40} \n [+] 登录账号 \n {'=' * 40} " )
-                instance.topLogin()
+                instance.topLogin_s()
                 time.sleep(2)
                 instance.topHome()
-                time.sleep(0.5)
+                time.sleep(2)
                 instance.lq001()
-                time.sleep(0.5)
-                instance.Present()
-                time.sleep(0.5)
                 instance.lq002()
                 time.sleep(2)
                 instance.buyBlueApple()
                 time.sleep(1)
                 instance.lq003()
                 time.sleep(1)
-                instance.LTO_Gacha()
-                time.sleep(1)
                 instance.drawFP()
-
 
             except Exception as ex:
                 logger.error(ex)
+
 
 if __name__ == "__main__":
     main()
